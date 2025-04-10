5c5d60db2e603b0701e0787ef1b021ce910b73b8
Ruturaj <ruturajnikam1010@gmail.com>
2025-04-11 01:09:58 +0530

    my first commit

---
+++ 6 Variables/variables.java
@@ -0,0 +1,8 @@
+public class variables {
+    public static void main(String args[]) {
+        int a = 10;
+        String name = "Ruturaj";
+
+        System.out.println("This is integer " + a + " and " + name +" this is string variable" );
+    }
+}
