c0eff78a3fbbb8f759a68ba9145e9b5e52b76e0c
Ruturaj <ruturajnikam1010@gmail.com>
2025-07-23 21:57:33 +0530

    factorial and optimized prime number code

---
+++ 9 Loops/factorial.java
@@ -0,0 +1,15 @@
+public class factorial {
+
+    public static void main(String args[]) {
+        int num = 5;
+        int fact = 1;
+
+        while (num > 0) {
+            fact = fact * num;
+            num--;
+        }
+
+        System.out.println("Factorial is: " + fact);
+
+    }
+}
--- 9 Loops/prime.java
+++ 9 Loops/prime.java
@@ -8,14 +8,14 @@ public class prime {
 
         boolean isPrime = true;
 
-        for (int i = 2; i <= (num/2); i++) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num%i == 0) {
                 isPrime = false;
                 break;
             }
         }
 
-        if (isPrime) {
+        if (isPrime || num == 2) {
             System.out.println(num + " is a prime number.");
         } else {
             System.out.println(num + " is not a prime number.");
