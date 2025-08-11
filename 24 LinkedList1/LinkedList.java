2ad60bd0b1b7ee506f5beed41fd90135a938ede4
Ruturaj <ruturajnikam1010@gmail.com>
2025-08-12 01:02:53 +0530

    performed remove operations

--- 24 LinkedList1/LinkedList.java
+++ 24 LinkedList1/LinkedList.java
@@ -36,7 +36,7 @@ public class LinkedList {
     }
 
     public void add(int idx, int data) {
-        if(idx == 0) {
+        if (idx == 0) {
             addFirst(data);
             return;
         }
@@ -45,13 +45,58 @@ public class LinkedList {
 
         Node temp = head;
         int i = 0;
-        while (i < idx-1) {
+        while (i < idx - 1) {
             temp = temp.next;
             i++;
         }
 
         newNode.next = temp.next;
         temp.next = newNode;
+    }
+
+    public int removeFirst() {
+        if (isEmpty(head)) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    public int removeLast() {
+
+        if (isEmpty(head)) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+
+        Node temp = head;
+
+        // this is also easy to understand the looping concept
+        // for (int i = 0; i < size-2; i++) {
+        //     temp = temp.next;
+        // }
+
+        while (temp.next != tail) {
+            temp = temp.next;
+        }
+        int val = tail.data;
+        temp.next = null;
+        tail = temp;
+        size--;
+        return val;
     }
 
     public static boolean isEmpty(Node head) {
@@ -73,6 +118,7 @@ public class LinkedList {
             temp = temp.next;
         }
         System.out.print("null");
+        System.out.println();
     }
 
     public static void main(String[] args) {
@@ -84,6 +130,9 @@ public class LinkedList {
         ll.add(2, 2);
 
         ll.printLL(head);
-        System.out.println("\n" + size);
+        // System.out.println(size);
+        // ll.removeFirst();
+        ll.removeLast();
+        ll.printLL(head);
     }
 }
