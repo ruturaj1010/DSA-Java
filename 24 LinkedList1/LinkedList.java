f75aaad71f1b88dcf202eff7923fb59d856ca57b
Ruturaj <ruturajnikam1010@gmail.com>
2025-08-11 23:42:24 +0530

    add node at index and calculate size of LL

--- 24 LinkedList1/LinkedList.java
+++ 24 LinkedList1/LinkedList.java
@@ -11,10 +11,11 @@ public class LinkedList {
 
     public static Node head;
     public static Node tail;
+    public static int size = 0;
 
     public void addFirst(int data) {
         Node newNode = new Node(data);
-
+        size++;
         if (isEmpty(head)) {
             head = tail = newNode;
             return;
@@ -25,13 +26,32 @@ public class LinkedList {
 
     public void addLast(int data) {
         Node newNode = new Node(data);
-
+        size++;
         if (isEmpty(head)) {
             head = tail = newNode;
             return;
         }
         tail.next = newNode;
         tail = newNode;
+    }
+
+    public void add(int idx, int data) {
+        if(idx == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+
+        Node temp = head;
+        int i = 0;
+        while (i < idx-1) {
+            temp = temp.next;
+            i++;
+        }
+
+        newNode.next = temp.next;
+        temp.next = newNode;
     }
 
     public static boolean isEmpty(Node head) {
@@ -57,11 +77,13 @@ public class LinkedList {
 
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.addFirst(0);
         ll.addFirst(1);
-        ll.addLast(-1);
-        ll.addLast(-2);
+        ll.addFirst(0);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.add(2, 2);
 
         ll.printLL(head);
+        System.out.println("\n" + size);
     }
 }
