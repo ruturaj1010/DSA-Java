a84d9cb4971840657d357bb323134bbe1a6f0410
Ruturaj <ruturajnikam1010@gmail.com>
2025-08-15 02:56:53 +0530

    implemented stack using LinkedList

---
+++ 26 Stacks/stackUsingLinkedList.java
@@ -0,0 +1,60 @@
+public class stackUsingLinkedList {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static class Stack {
+        public static Node head = null;
+
+        public boolean isEmpty() {
+            return head == null;
+        }
+
+        public void push(int data) {
+            Node newNode = new Node(data);
+            if (isEmpty()) {
+                head = newNode;
+                return;
+            }
+
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int val = head.data;
+            head = head.next;
+            return val;
+        }
+
+        public int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return head.data;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack st = new Stack();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+
+        while (!st.isEmpty()) {
+            System.out.print(st.peek() + " ");
+            st.pop();
+        }
+    }
+}
