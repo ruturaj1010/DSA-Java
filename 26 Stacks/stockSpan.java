ac22ce4b9a0ffcbc434e5f33d1f62c5e43b63e19
Ruturaj <ruturajnikam1010@gmail.com>
2025-08-17 00:58:13 +0530

    stock price span problem

---
+++ 26 Stacks/stockSpan.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+public class stockSpan {
+
+    public static void stockSpanResult(int stocks[], int span[]) {
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+
+        for (int i = 1; i < stocks.length; i++) {
+            int currPrice = stocks[i];
+            while (!s.isEmpty() && currPrice >= stocks[s.peek()]) {
+                s.pop();
+            }
+
+            if (s.empty()) {
+                span[i] = i+1;
+            } else {
+                int prevHighIdx = s.peek();
+                span[i] = i - prevHighIdx;
+            }
+
+            s.push(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int stocks[] = { 100, 80, 60, 70, 60, 85, 100 };
+        int span[] = new int[stocks.length];
+
+        stockSpanResult(stocks, span);
+
+        for (int i = 0; i < span.length; i++) {
+            System.out.print(span[i] + " ");
+        }
+        System.out.println();
+    }
+}
