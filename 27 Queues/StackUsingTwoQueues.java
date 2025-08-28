2a497e8ec55d4b4a907caee6830445927444383b
Ruturaj <ruturajnikam1010@gmail.com>
2025-08-28 11:41:39 +0530

    stack using two queues

---
+++ 27 Queues/StackUsingTwoQueues.java
@@ -0,0 +1,86 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class StackUsingTwoQueues {
+    static class Stack {
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+
+        public boolean isEmpty() {
+            return q1.isEmpty() && q2.isEmpty();
+        }
+
+        public void push(int data) {
+            if (!q1.isEmpty()) {
+                q1.add(data);
+            } else {
+                q2.add(data);
+            }
+        }
+
+        public int pop() {
+            if (isEmpty()) {
+                System.out.println("Stack is empty");
+                return -1;
+            }
+
+            int top = -1;
+
+            // case 1
+            if (!q1.isEmpty()) {
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+                    if (q1.isEmpty()) {
+                        break;
+                    }
+                    q2.add(top);
+                }
+            } else { // case 2
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    if (q2.isEmpty()) {
+                        break;
+                    }
+                    q1.add(top);
+                }
+            }
+
+            return top;
+        }
+
+        public int peek() {
+            if (isEmpty()) {
+                System.out.println("Stack is Empty");
+                return -1;
+            }
+
+            int top = -1;
+
+            if (!q1.isEmpty()) {
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+                    q2.add(top);
+                }
+            } else {
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    q1.add(top);
+                }
+            }
+
+            return top;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s1 = new Stack();
+        s1.push(1);
+        s1.push(2);
+        s1.push(3);
+
+        while (!s1.isEmpty()) {
+            System.out.println(s1.peek());
+            s1.pop();
+        }
+    }
+}
