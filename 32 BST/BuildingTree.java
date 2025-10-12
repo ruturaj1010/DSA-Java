94b0a37a93fb0eece78694d739676a632a6af144
Ruturaj <ruturajnikam1010@gmail.com>
2025-10-13 00:40:16 +0530

    deleting a node from BST

--- 32 BST/BuildingTree.java
+++ 32 BST/BuildingTree.java
@@ -52,6 +52,42 @@ public class BuildingTree {
         }
     }
 
+    public static Node delete (Node root, int val) {
+        
+        if (root.data < val) {
+            root.right = delete(root.right, val);
+        } else if (root.data > val) {
+            root.left = delete(root.left, val);
+        } else {
+
+            // case 1 no children
+            if (root.left == null && root.right ==  null) {
+                return null;
+            }
+
+            // case 2 with single child
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            // case 3 voila case having both childs
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+
+    public static Node findInorderSuccessor(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+
+        return root;
+    }
+
     public static void main(String[] args) {
         int values[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
 
@@ -63,5 +99,9 @@ public class BuildingTree {
         InOrder(root);
 
         System.out.println("\n" + (searchKey(root, 4) ? "Found" : "not found"));
+
+        root = delete(root, 3);
+
+        InOrder(root);
     }
 }
