cbb8bb8760133e86cd938265776ccfb31933da0a
Ruturaj <ruturajnikam1010@gmail.com>
2025-10-18 00:41:17 +0530

    Priority queues using JCF and Object data

--- 34 Heaps/PriorityQue.java
+++ 34 Heaps/PriorityQue.java
@@ -2,16 +2,45 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class PriorityQue {
-    public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        pq.add(3);
-        pq.add(7);
-        pq.add(1);
-        pq.add(4);
 
-        while (!pq.isEmpty()) {
-            System.out.print(pq.peek() + " ");
-            pq.remove();
+    static class Student implements Comparable<Student> {
+        String name;
+        int rank;
+
+        Student (String name, int rank) {
+            this.name = name;
+            this.rank = rank;
+        }
+
+        @Override
+        public int compareTo(Student S2) {
+            return this.rank - S2.rank;
+        }
+    }
+
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pa = new PriorityQueue<>(Comparator.reverseOrder());
+        pa.add(3);
+        pa.add(7);
+        pa.add(1);
+        pa.add(4);
+
+        while (!pa.isEmpty()) {
+            System.out.print(pa.peek() + " ");
+            pa.remove();
+        }
+        System.out.println();
+
+
+        PriorityQueue<Student> pb = new PriorityQueue<>();
+        pb.add(new Student("Prathmesh", 25));
+        pb.add(new Student("Yash", 17));
+        pb.add(new Student("Utkarsh", 9));
+        pb.add(new Student("Priyanka", 23));
+
+        while (!pb.isEmpty()) {
+            System.out.print(pb.peek().name + " " + pb.peek().rank + " -> ");
+            pb.remove();
         }
     }
 }
