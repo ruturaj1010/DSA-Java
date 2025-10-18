843cf33b5846aee13e43938c885087a9b9e8551a
Ruturaj <ruturajnikam1010@gmail.com>
2025-10-19 02:30:38 +0530

    fixed bugs

--- 34 Heaps/HeapsOp.java
+++ 34 Heaps/HeapsOp.java
@@ -12,7 +12,7 @@ public class HeapsOp {
             int x = h.size() - 1; // index of child node
             int par = (x - 1) / 2; // index of parent element
 
-            while (h.get(x) < h.get(par)) {
+            while (x > 0 && h.get(x) < h.get(par)) {
                 int temp = h.get(x);
                 h.set(x, h.get(par));
                 h.set(par, temp);
@@ -28,14 +28,14 @@ public class HeapsOp {
 
         private void heapify(int i) {
             int minIdx = i;
-            int left = 2*i + 1;
-            int right = 2*1 + 2;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
 
-            if (left < h.size()-1 && h.get(minIdx) > h.get(left)) {
+            if (left < h.size() && h.get(minIdx) > h.get(left)) {
                 minIdx = left;
             }
 
-            if (right < h.size()-1 && h.get(minIdx) > h.get(right)) {
+            if (right < h.size() && h.get(minIdx) > h.get(right)) {
                 minIdx = right;
             }
 
@@ -43,23 +43,25 @@ public class HeapsOp {
                 int temp = h.get(i);
                 h.set(i, h.get(minIdx));
                 h.set(minIdx, temp);
+                heapify(minIdx);
             }
         }
+
         public int remove() {
-            
+
             if (h.isEmpty()) {
                 return -1;
             }
 
             int data = h.get(0);
-            
+
             // Swap first and last ele
             int temp = h.get(0);
-            h.set(0, h.get(h.size()-1));
-            h.set(h.size()-1, temp);
+            h.set(0, h.get(h.size() - 1));
+            h.set(h.size() - 1, temp);
 
             // delete the last element
-            h.remove(h.size()-1);
+            h.remove(h.size() - 1);
 
             // heapify the heap tree
             heapify(0);
@@ -77,15 +79,10 @@ public class HeapsOp {
         h1.add(1);
         h1.add(5);
 
-        for (int i = 0; i < h1.h.size(); i++) {
-            System.out.print(h1.h.get(i) + " ");
+        while (!h1.h.isEmpty()) {
+            System.out.print(h1.peek() + " ");
+            h1.remove();
         }
-        System.out.println();
 
-        h1.remove();
-        h1.remove();
-        for (int i = 0; i < h1.h.size(); i++) {
-            System.out.print(h1.h.get(i) + " ");
-        }
     }
 }
