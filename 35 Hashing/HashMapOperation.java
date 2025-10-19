d59d98fb6a98a5ab4f4e4685570187f880dc834f
Ruturaj <ruturajnikam1010@gmail.com>
2025-10-20 02:51:06 +0530

    Iteration over HashMap

--- 35 Hashing/HashMapOperation.java
+++ 35 Hashing/HashMapOperation.java
@@ -1,6 +1,17 @@
 import java.util.HashMap;
+import java.util.Set;
 
 public class HashMapOperation {
+
+    public static void iterateOnHM(HashMap<String, Integer> hm) {
+        Set<String> values = hm.keySet();
+
+        System.out.println(values);
+
+        for (String k : values) {
+            System.out.println("Key = " + k + " , " + "Value = " + hm.get(k));
+        }
+    }
     public static void main(String[] args) {
         HashMap<String, Integer> hm = new HashMap<>();
 
@@ -10,31 +21,33 @@ public class HashMapOperation {
         hm.put("India", 150);
         hm.put("China", 120);
 
-        System.out.println(hm);
+        iterateOnHM(hm);
 
-        // Get - O(1)
-        int population = hm.get("US");
-        System.out.println(population);
+        // System.out.println(hm);
 
-        System.out.println(hm.get("Israel"));
+        // // Get - O(1)
+        // int population = hm.get("US");
+        // System.out.println(population);
 
-        // containsKey - O(1)
-        System.out.println(hm.containsKey("India"));
-        System.out.println(hm.containsKey("Aus"));
+        // System.out.println(hm.get("Israel"));
 
-        // Remove - O(1)
-        System.out.println(hm.remove("US"));
-        System.out.println(hm.remove("NZ"));
+        // // containsKey - O(1)
+        // System.out.println(hm.containsKey("India"));
+        // System.out.println(hm.containsKey("Aus"));
 
-        // Size
-        System.out.println(hm.size());
-        System.out.println(hm);
+        // // Remove - O(1)
+        // System.out.println(hm.remove("US"));
+        // System.out.println(hm.remove("NZ"));
 
-        // Empty
-        System.out.println(hm.isEmpty());
+        // // Size
+        // System.out.println(hm.size());
+        // System.out.println(hm);
 
-        // Clear Hashmap
-        hm.clear();
-        System.out.println(hm.size());
+        // // Empty
+        // System.out.println(hm.isEmpty());
+
+        // // Clear Hashmap
+        // hm.clear();
+        // System.out.println(hm.size());
     }
 }
