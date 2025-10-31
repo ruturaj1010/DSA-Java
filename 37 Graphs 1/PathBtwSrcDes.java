eb405d43f577870b1f136230ae68c627461e02cb
Ruturaj <ruturajnikam1010@gmail.com>
2025-11-01 01:57:22 +0530

    checking the path between vertices

---
+++ 37 Graphs 1/PathBtwSrcDes.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+
+public class PathBtwSrcDes {
+
+    static class Edge {
+        int src;
+        int des;
+        int wt = 0;
+
+        Edge(int s, int d, int w) {
+            this.src = s;
+            this.des = d;
+            this.wt = w;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
+
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
+
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+
+        graph[6].add(new Edge(6, 5, 1));
+    }
+
+    public static boolean hasPath(ArrayList<Edge> graph[] , int src, int des, boolean visi[]) {
+        if (src == des) {
+            return true;
+        }
+        visi[src] = true;
+
+        for (int i = 0; i < graph[src].size(); i++) {
+            Edge e = graph[src].get(i);
+            if (!visi[e.des] && hasPath(graph, e.des , des, visi)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int V = 7;
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge> graph[] = new ArrayList[V];
+
+        createGraph(graph);
+
+        System.out.println("Has a path : " + hasPath(graph, 0, 2, new boolean[V]));
+    }
+}
