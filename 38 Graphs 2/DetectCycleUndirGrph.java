029fa13acae8858605faa424e65ca978f39f89ab
Ruturaj <ruturajnikam1010@gmail.com>
2025-11-02 00:40:35 +0530

    detect cycle in undirected graph

---
+++ 38 Graphs 2/DetectCycleUndirGrph.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+
+public class DetectCycleUndirGrph {
+
+    static class Edge {
+        int src;
+        int des;
+
+        Edge(int s, int d) {
+            this.src = s;
+            this.des = d;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // graph[0].add(new Edge(0, 1));
+        // graph[0].add(new Edge(0, 2));
+        // graph[0].add(new Edge(0, 3));
+
+        // graph[1].add(new Edge(1, 0));
+        // graph[1].add(new Edge(1, 2));
+
+        // graph[2].add(new Edge(2, 0));
+        // graph[2].add(new Edge(2, 1));
+
+        // graph[3].add(new Edge(3, 0));
+        // graph[3].add(new Edge(3, 4));
+        
+        // graph[4].add(new Edge(4, 3));
+
+
+        // Graph without cycle 
+
+        graph[0].add(new Edge(0, 1));
+        graph[1].add(new Edge(1, 0));
+
+        graph[1].add(new Edge(1, 2));
+        graph[2].add(new Edge(2, 1));
+
+        graph[1].add(new Edge(1, 3));
+        graph[3].add(new Edge(3, 1));
+
+        graph[3].add(new Edge(3, 4));
+        graph[4].add(new Edge(4, 3));
+    }
+
+    public static boolean detectCycle(ArrayList<Edge> graph[]) {
+        boolean vis[] = new boolean[graph.length];
+
+        for (int i = 0; i < graph.length; i++) {
+            if (!vis[i]) {
+                if (detectCycleUtil(graph, vis, i, -1)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean detectCycleUtil(ArrayList<Edge> graph[], boolean vis[], int curr, int par) {
+        vis[curr] = true;
+
+        for (Edge e : graph[curr]) {
+            if (!vis[e.des]) {
+                if (detectCycleUtil(graph, vis, e.des, curr))
+                    return true;
+            } else if (e.des != par) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int V = 5;
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge> graph[] = new ArrayList[V];
+
+        createGraph(graph);
+
+        System.out.println("Is there a cycle in graph : " + detectCycle(graph));
+    }
+}
